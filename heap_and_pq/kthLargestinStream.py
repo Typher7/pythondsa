4578e5ea594bdfb4678590034e11c3d76536b3aa
Enoch Atuahene <eatuahene@springfieldcollege.edu>
2024-02-13 15:06:09 -0500

    init method and add method for min heap added

--- heap_and_pq/kthLargestinStream.py
+++ heap_and_pq/kthLargestinStream.py
@@ -1,7 +1,17 @@
 import heapq
 class KthLargest:
     def __init__(self, k, numbers):
-        self.minHeap, self.k = heapq.heapify(numbers)
+        self.minHeap, self.k = numbers, k
+        heapq.heapify(self.minHeap)
+        
+        while len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+
 
     def add(self, val):
-        pass
+        heapq.heappush(self.minHeap, val)
+        while len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+
+        #return kth largest after popping
+        return self.minHeap[0]
